6b42170a37a75b240bb6eb6f4bca539fb053caab
where.liu <where.liu@huolala.cn>
2021-11-27 17:49:52 +0800

    feat: resize

--- src/pages/Json.tsx
+++ src/pages/Json.tsx
@@ -1,6 +1,31 @@
-import React from 'react';
-import {Box} from '@mui/material';
+import React, {useRef, useState, useEffect} from 'react';
+import {Box, Container} from '@mui/material';
+import JSONEditor, {JSONEditorOptions} from 'jsoneditor';
+
+import 'jsoneditor/dist/jsoneditor.css';
+
+const options: JSONEditorOptions = {
+  language: 'zh-CN',
+  mode: 'code',
+};
 
 export default function Json() {
-  return <Box>aaa</Box>;
+  const editorRef = useRef<HTMLDivElement>();
+  const [editorHeight, setEditorHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    if (editorRef.current) {
+      new JSONEditor(editorRef.current, options);
+    }
+    function re() {
+      setEditorHeight(window.innerHeight);
+    }
+    window.addEventListener('resize', re);
+  }, []);
+
+  return (
+    <Container>
+      <Box ref={editorRef} height={editorHeight} />
+    </Container>
+  );
 }
