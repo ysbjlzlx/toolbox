4ea2e0cae1ef27bf4053f588dbada42a5db9327f
where.liu <where.liu@huolala.cn>
2021-11-29 13:25:39 +0800

    feat: save

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Box, Typography} from '@mui/material';
+import {Container, Typography} from '@mui/material';
 
 export default function PageNotFound() {
   return (
-    <Box>
+    <Container>
       <Typography>Page Not Found.</Typography>
-    </Box>
+    </Container>
   );
 }
