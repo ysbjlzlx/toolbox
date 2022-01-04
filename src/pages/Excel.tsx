10f83cb2009689a112dc8ad5cc2a11e0e119d729
where.liu <where.liu@huolala.cn>
2022-01-04 16:10:00 +0800

    feat: save

--- src/pages/Excel.tsx
+++ src/pages/Excel.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import {Box, Container, Typography, Button} from '@mui/material';
-
+import xlsx from 'xlsx';
 import {styled} from '@mui/material/styles';
 
 const Input = styled('input')({
@@ -8,8 +8,30 @@ const Input = styled('input')({
 });
 
 export default function Excel() {
-  const onUpload = (event: any) => {
+  const onUpload = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.files);
     console.log(event.target.value);
+    const files = event.target.files;
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        const fileReader = new FileReader();
+        fileReader.onload = function (e: ProgressEvent<FileReader>) {
+          if (!e.target) {
+            return;
+          }
+          let data = e.target.result as ArrayBuffer;
+          data = new Uint8Array(data);
+          const workbook = xlsx.read(data, {type: 'array'});
+          console.log(workbook);
+          /* DO SOMETHING WITH workbook HERE */
+        };
+
+        const file = files.item(i);
+        if (file) {
+          fileReader.readAsArrayBuffer(file);
+        }
+      }
+    }
   };
   return (
     <Container>
