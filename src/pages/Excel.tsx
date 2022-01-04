f33e19de1126fd09ed0134a81a69e079a4b17744
where.liu <where.liu@huolala.cn>
2022-01-04 18:06:32 +0800

    feat: save

--- src/pages/Excel.tsx
+++ src/pages/Excel.tsx
@@ -1,13 +1,17 @@
-import React, {ChangeEvent} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import {Box, Container, Typography, Button} from '@mui/material';
 import xlsx from 'xlsx';
 import {styled} from '@mui/material/styles';
+import {DataGrid} from '@mui/x-data-grid';
 
 const Input = styled('input')({
   display: 'none',
 });
 
 export default function Excel() {
+  const [columns, setColumns] = useState([]);
+  const [rows, setRows] = useState([]);
+
   const onUpload = (event: ChangeEvent<HTMLInputElement>) => {
     console.log(event.target.files);
     console.log(event.target.value);
@@ -15,7 +19,7 @@ export default function Excel() {
     if (files) {
       for (let i = 0; i < files.length; i++) {
         const fileReader = new FileReader();
-        fileReader.onload = function (e: ProgressEvent<FileReader>) {
+        fileReader.onload = (e: ProgressEvent<FileReader>) => {
           if (!e.target) {
             return;
           }
@@ -23,6 +27,17 @@ export default function Excel() {
           data = new Uint8Array(data);
           const workbook = xlsx.read(data, {type: 'array'});
           console.log(workbook);
+          const firstSheet = workbook.Sheets[workbook.SheetNames[0]];
+          const rows = xlsx.utils.sheet_to_json(firstSheet) || [];
+          if (rows) {
+            // @ts-ignore
+            for (const [key, value] of Object.entries(rows[0])) {
+              console.log(`${key}: ${value}`);
+            }
+          }
+          // @ts-ignore
+          setRows(rows);
+          console.log(firstSheet['!ref']);
           /* DO SOMETHING WITH workbook HERE */
         };
 
@@ -36,9 +51,9 @@ export default function Excel() {
   return (
     <Container>
       <Box sx={{mt: 2}}>
-        <Typography>Excel</Typography>
+        <Typography variant={'h2'}>Excel 转 JSON</Typography>
       </Box>
-      <Box>
+      <Box sx={{mt: 2}}>
         <label htmlFor="contained-button-file">
           <Input
             accept=".xls,.xlsx,.csv"
@@ -48,9 +63,19 @@ export default function Excel() {
             onChange={onUpload}
           />
           <Button variant="contained" component="span">
-            Upload
+            上传 Excel
           </Button>
         </label>
+      </Box>
+      <Box sx={{mt: 2}}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={10}
+          rowsPerPageOptions={[10, 20, 50, 100]}
+          disableSelectionOnClick
+          autoHeight
+        />
       </Box>
     </Container>
   );
