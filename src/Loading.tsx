35eb7cb4d5f894b02baa6ccbde7e07f2d9811c1f
where.liu <where.liu@huolala.cn>
2022-02-23 09:58:51 +0800

    feat: save

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import {Box, CircularProgress, Stack} from '@mui/material';
 
 export default () => {
-  return <div>加载中...</div>;
+  return (
+    <Stack
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      spacing={2}
+      sx={{height: '100vh'}}
+    >
+      <Box>
+        <CircularProgress />
+      </Box>
+      <Box>
+        <div>加载中...</div>
+      </Box>
+    </Stack>
+  );
 };
