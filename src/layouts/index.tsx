501cfc70902cb92bc17b0b5518e8b2929c76696a
where.liu <where.liu@huolala.cn>
2022-02-23 10:48:11 +0800

    feat: layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,13 +1,15 @@
 import React, {FC} from 'react';
-import {CssBaseline} from '@mui/material';
+import {Box, CssBaseline} from '@mui/material';
 
-export default function Baselayout(props: any) {
+const BaseLayout: FC = ({children}) => {
   return (
     <React.StrictMode>
       <React.Fragment>
         <CssBaseline />
-        <div>{props.children}</div>
+        <Box>{children}</Box>
       </React.Fragment>
     </React.StrictMode>
   );
-}
+};
+
+export default BaseLayout;
