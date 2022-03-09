1d8edfa8b70d16fb614032f0dacb1096b03df42c
Where.LIU <liuzhaowei55@gmail.com>
2022-03-09 11:16:17 +0800

    feat: save

--- src/pages/Markdown.tsx
+++ src/pages/Markdown.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
-import {Container} from '@mui/material';
-import {Editor, rootCtx} from '@milkdown/core';
-import {ReactEditor, useEditor} from '@milkdown/react';
-import {nord} from '@milkdown/theme-nord';
-import {commonmark} from '@milkdown/preset-commonmark';
-import {history} from '@milkdown/plugin-history';
+import { Container } from '@mui/material';
+import { Editor, rootCtx } from '@milkdown/core';
+import { ReactEditor, useEditor } from '@milkdown/react';
+import { nord } from '@milkdown/theme-nord';
+import { commonmark } from '@milkdown/preset-commonmark';
+import { history } from '@milkdown/plugin-history';
+import { clipboard } from '@milkdown/plugin-clipboard';
+import { tooltip } from '@milkdown/plugin-tooltip';
+import { prism } from '@milkdown/plugin-prism';
 
 export default function Markdown() {
-  const editor = useEditor(root => {
+  const editor = useEditor((root) => {
     return Editor.make()
-      .config(ctx => {
+      .config((ctx) => {
         ctx.set(rootCtx, root);
       })
       .use(nord)
       .use(commonmark)
-      .use(history);
+      .use(history)
+      .use(clipboard)
+      .use(tooltip)
+      .use(prism);
   });
   return (
-    <Container sx={{pt: 2}}>
+    <Container sx={{ mt: 2 }}>
       <ReactEditor editor={editor} />
     </Container>
   );
