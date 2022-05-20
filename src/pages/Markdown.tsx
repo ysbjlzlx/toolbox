9040d887c17636cd7a38b25825f0d6d4b10c90a3
where.liu <where.liu@huolala.cn>
2022-05-20 12:15:32 +0800

    feat: security

--- src/pages/Markdown.tsx
+++ src/pages/Markdown.tsx
@@ -1,30 +1,10 @@
 import React from 'react';
-import { Container } from '@mui/material';
-import { Editor, rootCtx } from '@milkdown/core';
-import { ReactEditor, useEditor } from '@milkdown/react';
-import { nord } from '@milkdown/theme-nord';
-import { commonmark } from '@milkdown/preset-commonmark';
-import { history } from '@milkdown/plugin-history';
-import { clipboard } from '@milkdown/plugin-clipboard';
-import { tooltip } from '@milkdown/plugin-tooltip';
-import { prism } from '@milkdown/plugin-prism';
+import { Container, TextField } from '@mui/material';
 
 export default function Markdown() {
-  const editor = useEditor((root) => {
-    return Editor.make()
-      .config((ctx) => {
-        ctx.set(rootCtx, root);
-      })
-      .use(nord)
-      .use(commonmark)
-      .use(history)
-      .use(clipboard)
-      .use(tooltip)
-      .use(prism);
-  });
   return (
     <Container sx={{ mt: 2 }}>
-      <ReactEditor editor={editor} />
+      <TextField type={'text'} minRows={5} maxRows={10} multiline fullWidth />
     </Container>
   );
 }
