1385e8202df2585c6e1fbe13450add828290faad
Where <liuzhaowei55@gmail.com>
2022-05-20 21:29:46 +0800

    feat: markdown

--- src/pages/Markdown.tsx
+++ src/pages/Markdown.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
-import { Container, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Grid } from '@mui/material';
+import AceEditor from 'react-ace';
+import { AceOptions } from 'react-ace/types';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+import 'ace-builds/src-min-noconflict/mode-markdown';
+import 'ace-builds/src-min-noconflict/ext-searchbox';
+import 'ace-builds/src-min-noconflict/theme-textmate';
 
 export default function Markdown() {
+  const [markdown, setMarkdown] = useState<string>('');
+
+  const markdownOptions: AceOptions = {
+    useWorker: false,
+    showLineNumbers: true,
+    wrap: true,
+    fontSize: 14,
+  };
   return (
-    <Container sx={{ mt: 2 }}>
-      <TextField type={'text'} minRows={5} maxRows={10} multiline fullWidth />
-    </Container>
+    <Box sx={{ height: '100%' }}>
+      <Grid container sx={{ height: '100%' }}>
+        <Grid item xs={12} md={6} sx={{ height: '100%' }}>
+          <AceEditor
+            theme="textmate"
+            placeholder="Input"
+            setOptions={markdownOptions}
+            value={markdown}
+            onChange={setMarkdown}
+            mode="markdown"
+            width="100%"
+            height="calc(100% - 37px)"
+          />
+        </Grid>
+        <Grid item xs={12} md={6} sx={{ height: '100%' }}>
+          <Box height="calc(100% - 37px)" sx={{ width: '100%', px: 1 }}>
+            <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
