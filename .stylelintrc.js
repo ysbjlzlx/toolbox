890f2a1ed5be671fb5af8700f88d3e4717222172
where.liu <where.liu@huolala.cn>
2022-06-27 14:10:54 +0800

    feat: update package

---
+++ .stylelintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // extends: [require.resolve('@umijs/fabric/dist/stylelint')],
+  extends: [require.resolve('@umijs/lint/dist/config/stylelint')],
+  rules: {
+    // your rules
+  },
+};
