3504c2e066798adfb86347be44aaf02e06020464
liuzhaowei55 <liuzhaowei55@gmail.com>
2022-07-14 21:59:33 +0800

    feat: json update

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ['react-app', 'react-app/jest'],
   // extends: [require.resolve('@umijs/fabric/dist/eslint')],
   // extends: [require.resolve('@umijs/lint/dist/config/eslint')],
-
+  plugins: ['react-hooks'],
   rules: {
     // your rules
   },
