997e4589f4b20560b07f38de49bb12d78373c0e3
liuzhaowei55 <liuzhaowei55@gmail.com>
2022-07-21 23:42:13 +0800

    feat: 占位图

--- src/components/placeholder/HrefLuImage.tsx
+++ src/components/placeholder/HrefLuImage.tsx
@@ -1,48 +1,14 @@
-import _ from 'lodash';
 import Image from 'mui-image';
 import { useEffect, useState } from 'react';
 import { PlaceholderConfig } from './index';
-
-const defaultConfig: PlaceholderConfig = {
-  width: 150,
-  height: 150,
-  suffix: '.png',
-  bgColor: '#cccccc',
-  textColor: '#969696',
-  text: '',
-};
+import { defaultConfig, resolveConfig } from './util';
 
 const HrefLuImage = (input: PlaceholderConfig) => {
   const [imgUrl, setImgUrl] = useState<string>('');
-  const [config, setConfig] = useState<PlaceholderConfig>(defaultConfig);
+  const [config, setConfig] = useState<PlaceholderConfig>(defaultConfig());
 
   useEffect(() => {
-    setConfig((oldVal) => {
-      const newVal = _.clone(oldVal);
-      if (input.width && input.width > 0) {
-        newVal.width = input.width;
-      }
-      if (input.height && input.height > 0) {
-        newVal.height = input.height;
-      } else {
-        newVal.height = defaultConfig.height;
-      }
-      if (input.suffix) {
-        newVal.suffix = input.suffix;
-      }
-      if (input.bgColor) {
-        newVal.bgColor = input.bgColor;
-      }
-      if (input.textColor) {
-        newVal.textColor = input.textColor;
-      }
-      if (input.text && input.text !== '') {
-        newVal.text = input.text;
-      } else {
-        newVal.text = '';
-      }
-      return newVal;
-    });
+    setConfig(resolveConfig(input));
   }, [input]);
 
   useEffect(() => {
---
+++ src/components/placeholder/util.ts
@@ -0,0 +1,37 @@
+import { PlaceholderConfig } from './index';
+
+const defaultConfig = (): PlaceholderConfig => {
+  return {
+    width: 150,
+    height: 150,
+    suffix: '.png',
+    bgColor: '#cccccc',
+    textColor: '#969696',
+    text: '',
+  };
+};
+
+const resolveConfig = (input: PlaceholderConfig): PlaceholderConfig => {
+  const config: PlaceholderConfig = defaultConfig();
+  if (input.width && input.width > 0) {
+    config.width = input.width;
+  }
+  if (input.height && input.height > 0) {
+    config.height = input.height;
+  }
+  if (input.suffix) {
+    config.suffix = input.suffix;
+  }
+  if (input.bgColor) {
+    config.bgColor = input.bgColor;
+  }
+  if (input.textColor) {
+    config.textColor = input.textColor;
+  }
+  if (input.text && input.text !== '') {
+    config.text = input.text;
+  }
+  return config;
+};
+
+export { defaultConfig, resolveConfig };
