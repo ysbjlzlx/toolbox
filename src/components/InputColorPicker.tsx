0ec623edece3b9984158dfa353b42e8e3eac6a49
liuzhaowei55 <liuzhaowei55@gmail.com>
2022-07-30 23:00:01 +0800

    feat: input color picker

--- src/components/InputColorPicker.tsx
+++ src/components/InputColorPicker.tsx
@@ -1,15 +1,15 @@
-import { Box, Popover, TextField } from '@mui/material';
+import { Box, Popover, TextField, TextFieldProps } from '@mui/material';
 import { bindPopover, bindTrigger, usePopupState } from 'material-ui-popup-state/hooks';
 import React, { ChangeEvent } from 'react';
 import { GithubPicker, TwitterPicker } from 'react-color';
 
-interface InputColorPickerProps {
+interface Props {
   value: string;
   onChange: (val: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
-  label?: string;
-  fullWidth?: boolean;
   picker: 'GithubPicker' | 'TwitterPicker';
 }
+
+export type InputColorPickerProps = TextFieldProps & Props;
 
 const InputColorPicker = React.forwardRef((props: InputColorPickerProps, ref: any) => {
   const { value, onChange, picker, ...other } = props;
@@ -21,13 +21,7 @@ const InputColorPicker = React.forwardRef((props: InputColorPickerProps, ref: an
 
   return (
     <Box>
-      <TextField
-        ref={ref}
-        value={value}
-        onChange={(event) => onChange(event)}
-        {...other}
-        {...bindTrigger(popupState)}
-      />
+      <TextField ref={ref} value={value} onChange={onChange} {...other} {...bindTrigger(popupState)} />
       <Popover {...bindPopover(popupState)} anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}>
         {picker === 'GithubPicker' && (
           <Box sx={{ mt: '6px' }}>
