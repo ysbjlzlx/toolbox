7d7c4d3aa4370a576cbcf62d0395e52dca329b5b
liuzhaowei55 <liuzhaowei55@gmail.com>
2022-08-01 20:28:43 +0800

    feat: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA()],
   build: {
     sourcemap: true,
     outDir: 'build',
