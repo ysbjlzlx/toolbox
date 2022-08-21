589387cb82f4445c2f10d50eeb08ef992cbe8112
Where <liuzhaowei55@gmail.com>
2022-08-22 00:10:11 +0800

    feat: json localstorage

--- src/pages/JSON.tsx
+++ src/pages/JSON.tsx
@@ -1,9 +1,48 @@
+import { useLocalStorageState } from 'ahooks';
 import { FC } from 'react';
-import { Mode } from 'vanilla-jsoneditor';
+
+import { Content, Mode } from 'vanilla-jsoneditor';
 import VanillaJSONEditor from '../components/VanillaJSONEditor';
 
 const JSONBeta: FC = () => {
-  return <VanillaJSONEditor mode={Mode.text} content={{ text: '' }} />;
+  const defaultValue = {
+    mode: 'text',
+    content: {
+      text: '',
+    },
+  };
+  const [jsonStorage, setJsonStorage] = useLocalStorageState<any>('json', { defaultValue });
+
+  const resolveMode = (): Mode | undefined => {
+    if (jsonStorage && jsonStorage.mode === 'text') {
+      return Mode.text;
+    }
+    if (jsonStorage && jsonStorage.mode === 'tree') {
+      return Mode.tree;
+    }
+    return Mode.text;
+  };
+
+  const onChange = (content: Content) => {
+    setJsonStorage((oldValue: any) => {
+      return { ...oldValue, content: content };
+    });
+  };
+
+  const onChangMode = (mode: Mode) => {
+    setJsonStorage((oldValue: any) => {
+      return { ...oldValue, mode: mode };
+    });
+  };
+
+  return (
+    <VanillaJSONEditor
+      mode={resolveMode()}
+      content={jsonStorage.content}
+      onChange={onChange}
+      onChangeMode={onChangMode}
+    />
+  );
 };
 
 export default JSONBeta;
