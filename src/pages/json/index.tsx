25f733f7f2fb81a117e32430e75105c0b74dc69c
liuzhaowei55 <liuzhaowei55@gmail.com>
2022-10-01 01:23:45 +0800

    feat: json 标签

--- src/pages/json/index.tsx
+++ src/pages/json/index.tsx
@@ -3,6 +3,7 @@ import React, { useCallback, useState } from 'react';
 
 import TabPanel from '../../components/TabPanel';
 import JSON from '../JSON';
+import JSON2Yaml from '../JSON2Yaml';
 
 const Json = () => {
   const [currentTab, setCurrentTab] = useState<number>(0);
@@ -28,11 +29,16 @@ const Json = () => {
       <div ref={tabWrapperRef}>
         <Tabs value={currentTab} onChange={handleChange} aria-label="json tab panel">
           <Tab label="Json Editor" {...a11yProps(0)} />
+          <Tab label="Json To Yaml" {...a11yProps(1)} />
         </Tabs>
       </div>
 
       <TabPanel value={currentTab} index={0} sx={{ height: `calc(100% - ${tabWrapperHeight}px)` }}>
         <JSON />
+      </TabPanel>
+
+      <TabPanel value={currentTab} index={1} sx={{ height: `calc(100% - ${tabWrapperHeight}px)` }}>
+        <JSON2Yaml />
       </TabPanel>
     </>
   );
