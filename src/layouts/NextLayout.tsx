b08891a28b8af396edff423a99b83be1cdd77280
liuzhaowei55 <liuzhaowei55@gmail.com>
2022-12-14 23:18:35 +0800

    feat: sidebar

--- src/layouts/NextLayout.tsx
+++ src/layouts/NextLayout.tsx
@@ -1,24 +1,26 @@
-import { FC, useEffect, useState } from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
-
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
-
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-
-import './next-layout.css';
+import { Menu } from 'antd';
+import { ItemType } from 'antd/es/menu/hooks/useItems';
+import { FC, useEffect, useLayoutEffect, useState } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 import { apps } from '../models/Apps';
+import './next-layout.css';
 
 const NextLayout: FC = () => {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.up('md'));
   const [drawerWidth, setDrawerWidth] = useState<number>(200);
+  const [keyPath, setKeyPath] = useState<string[]>([]);
+  const navigate = useNavigate();
+  const location = useLocation();
+  useLayoutEffect(() => {
+    console.log(location.pathname);
+    setKeyPath([location.pathname]);
+  }, [location]);
   useEffect(() => {
     if (matches) {
       setDrawerWidth(200);
@@ -26,6 +28,15 @@ const NextLayout: FC = () => {
       setDrawerWidth(0);
     }
   }, [matches]);
+
+  const items = (): ItemType[] => {
+    const items: ItemType[] = [];
+    apps.forEach((app) => {
+      items.push({ label: app.name, key: app.href });
+    });
+
+    return items;
+  };
 
   return (
     <Box sx={{ display: 'flex', height: '100%' }}>
@@ -42,19 +53,15 @@ const NextLayout: FC = () => {
           },
         }}
       >
-        <Box sx={{ flexGrow: 1, backgroundColor: 'background.paper' }}>
-          <List>
-            {apps.map((app) => {
-              return (
-                <ListItem disablePadding key={app.name}>
-                  <ListItemButton component={NavLink} to={app.href}>
-                    <ListItemText primary={app.name} />
-                  </ListItemButton>
-                </ListItem>
-              );
-            })}
-          </List>
-        </Box>
+        <Menu
+          items={items()}
+          mode="inline"
+          selectedKeys={keyPath}
+          onClick={(e) => {
+            navigate(e.key);
+            console.log(e);
+          }}
+        />
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, width: { sm: `calc(100% - ${drawerWidth}px)` }, height: '100%' }}>
         <Outlet />
