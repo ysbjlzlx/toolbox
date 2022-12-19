6c9675d3f2f4dcec5d17fdbd9e913040cd1948a1
liuzhaowei55 <liuzhaowei55@gmail.com>
2022-12-19 20:33:17 +0800

    feat: random

--- src/pages/Random.tsx
+++ src/pages/Random.tsx
@@ -1,137 +1,98 @@
-import { Box, Container, TextField } from '@mui/material';
+import { Container } from '@mui/material';
 import { useEffect, useState } from 'react';
 
-import { Checkbox, Col, Form, Input, Row, Slider } from 'antd';
+import { Refresh } from '@mui/icons-material';
+import { Button, Card, Checkbox, Col, Form, Input, Row, Slider } from 'antd';
 import { Block } from '../components/Block';
 import ContentCopyAdornment from '../components/ContentCopyAdornment';
-import RefreshAdornment from '../components/RefreshAdornment';
 import { generator, GeneratorOptions } from '../scripts/RandomStringUtils';
 
+const initialOptions: GeneratorOptions = {
+  numeric: true,
+  lowercase: true,
+  uppercase: true,
+  symbol: false,
+  symbolChars: '!@#$%^&*',
+};
 export default function Random() {
   const [randomString, setRandomString] = useState<string>('');
-  const [numeric, setNumeric] = useState<boolean>(true);
-  const [upperCase, setUpperCase] = useState<boolean>(true);
-  const [lowerCase, setLowerCase] = useState<boolean>(true);
-  const [symbol, setSymbol] = useState<boolean>(false);
-  const [length, setLength] = useState<number>(8);
-  const [symbolChars, setSymbolChars] = useState<string>('!@#$%^&*');
-  const [options, setOptions] = useState<GeneratorOptions>({});
-  useEffect(() => {
-    setOptions({
-      length: length,
-      numeric: numeric,
-      lowercase: lowerCase,
-      uppercase: upperCase,
-      symbol: symbol,
-      symbolChars: symbolChars,
-    });
-  }, [length, numeric, lowerCase, upperCase, symbol, symbolChars]);
 
-  const refresh = () => {
+  useEffect(() => {
+    refresh();
+  }, []);
+
+  const refresh = (options: GeneratorOptions = initialOptions) => {
     const varchar = generator(options);
     setRandomString(varchar);
   };
 
-  useEffect(refresh, [length, numeric, upperCase, lowerCase, symbol, symbolChars, options]);
+  const onValuesChange = (changedValues: any, allValues: GeneratorOptions) => {
+    console.log(changedValues);
+    console.log(allValues);
+    const varchar = generator(allValues);
+    setRandomString(varchar);
+  };
 
-  const label = { inputProps: { 'aria-label': 'Checkbox' } };
+  const onFinish = (values: GeneratorOptions) => {
+    console.log(values);
+    refresh(values);
+  };
 
   return (
     <Container>
       <Block sx={{ mt: 2 }}>
-        <Form layout="horizontal">
+        <Form layout="horizontal" initialValues={initialOptions} onValuesChange={onValuesChange} onFinish={onFinish}>
+          <Row>
+            <Col span={4}>
+              <Form.Item name="numeric" valuePropName="checked">
+                <Checkbox>数字 0 ～ 9</Checkbox>
+              </Form.Item>
+            </Col>
+            <Col span={4}>
+              <Form.Item name="lowercase" valuePropName="checked">
+                <Checkbox>小写字母 a ~ z</Checkbox>
+              </Form.Item>
+            </Col>
+            <Col span={4}>
+              <Form.Item name="uppercase" valuePropName="checked">
+                <Checkbox>大写字母 A ~ Z</Checkbox>
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              <Row>
+                <Col span={4}>
+                  <Form.Item name="symbol" valuePropName="checked">
+                    <Checkbox>符号</Checkbox>
+                  </Form.Item>
+                </Col>
+                <Col span={20}>
+                  <Form.Item name="symbolChars">
+                    <Input />
+                  </Form.Item>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
           <Row>
             <Col span={24}>
-              <Form.Item>
-                <Checkbox
-                  checked={numeric}
-                  onChange={() => {
-                    setNumeric(!numeric);
-                  }}
-                >
-                  数字 0 ～ 9
-                </Checkbox>
-              </Form.Item>
-              <Form.Item>
-                <Checkbox
-                  checked={lowerCase}
-                  onChange={() => {
-                    setLowerCase(!lowerCase);
-                  }}
-                >
-                  小写字母 a ~ z
-                </Checkbox>
-              </Form.Item>
-              <Form.Item>
-                <Checkbox
-                  checked={upperCase}
-                  onChange={() => {
-                    setUpperCase(!upperCase);
-                  }}
-                >
-                  大写字母 A ~ Z
-                </Checkbox>
-              </Form.Item>
-              <Form.Item>
-                <Checkbox
-                  checked={symbol}
-                  onChange={() => {
-                    setSymbol(!symbol);
-                  }}
-                >
-                  符号
-                </Checkbox>
-              </Form.Item>
-              <Form.Item>
-                <Input
-                  disabled={!symbol}
-                  value={symbolChars}
-                  onChange={(event) => {
-                    setSymbolChars(event.target.value);
-                  }}
-                />
+              <Form.Item name="length" label="长度">
+                <Slider min={6} max={64} step={1} marks={{ 6: '6', 8: '8', 16: '16', 32: '32', 64: '64' }} />
               </Form.Item>
             </Col>
           </Row>
           <Row>
             <Col span={24}>
               <Form.Item>
-                <Slider
-                  value={length}
-                  min={6}
-                  max={64}
-                  step={1}
-                  marks={{ 6: '6', 8: '8', 16: '16', 32: '32', 64: '64' }}
-                  onChange={(value) => {
-                    if (typeof value === 'number') {
-                      setLength(value);
-                    }
-                  }}
-                />
+                <Card actions={[<Button key="1" htmlType="submit" icon={<Refresh fontSize="small" />} />]}>
+                  <Input.Group compact>
+                    <Input value={randomString} style={{ width: 'calc(100% - 60px)' }} />
+                    <ContentCopyAdornment content={randomString} />
+                  </Input.Group>
+                </Card>
               </Form.Item>
             </Col>
           </Row>
         </Form>
-
-        <Box sx={{ mt: 2 }}>
-          <TextField
-            variant="outlined"
-            label="随机结果"
-            value={randomString}
-            InputProps={{
-              endAdornment: (
-                <>
-                  <RefreshAdornment onClick={refresh} />
-                  <ContentCopyAdornment content={randomString} />
-                </>
-              ),
-            }}
-            onChange={(event) => {
-              setRandomString(event.target.value);
-            }}
-            fullWidth
-          />
-        </Box>
       </Block>
     </Container>
   );
