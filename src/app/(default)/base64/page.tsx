7b35f576f7232981b28ba69f6b6ab9ad0eee8800
liuzhaowei55 <liuzhaowei55@gmail.com>
2023-06-13 23:02:59 +0800

    fix: encode

--- src/app/(default)/base64/page.tsx
+++ src/app/(default)/base64/page.tsx
@@ -4,60 +4,39 @@ import { PageContainer } from '@ant-design/pro-components';
 import { Box } from '@mui/material';
 import { Button, Input, Segmented, Space, message } from 'antd';
 import { SegmentedLabeledOption } from 'antd/es/segmented';
-import { enc } from 'crypto-js';
 import { SegmentedRawOption } from 'rc-segmented';
 import { useCallback, useState } from 'react';
-// @ts-ignore
-// @ts-ignore
-import basex from 'base-x';
-import bs58 from 'bs58';
-import bs58check from 'bs58check';
+
+import { BaseType, decode as baseDecode, encode as baseEncode } from '@/lib/BaseEncoder';
 
 import TextResultBox from '../../../components/TextResultBox';
 
-type BaseType = 'base16' | 'base32' | 'base58' | 'base64' | 'base85';
-
 export default function Page() {
-  const [type, setType] = useState<BaseType>('base64');
+  const [type, setType] = useState<BaseType>('base-64');
   const [plain, setPlain] = useState<string>('');
   const [target, setTarget] = useState<string>('');
 
   const [messageApi, contextHolder] = message.useMessage();
 
   const segmentedOptions: (SegmentedRawOption | SegmentedLabeledOption)[] = [
-    { label: 'Base16', value: 'base16' },
-    { label: 'Base32', value: 'base32', disabled: true },
-    { label: 'Base58', value: 'base58', disabled: false },
-    { label: 'Base64', value: 'base64' },
-    { label: 'Base85', value: 'base85', disabled: true },
+    { label: 'Base16', value: 'base-16' },
+    { label: 'Base32', value: 'base-32', disabled: false },
+    { label: 'Z Base32', value: 'z-base-32', disabled: false },
+    { label: 'Base36', value: 'base-36', disabled: false },
+    { label: 'Base58', value: 'base-58', disabled: false },
+    { label: 'Base58 Check', value: 'base-58-check', disabled: false },
+    { label: 'Base64', value: 'base-64' },
+    { label: 'Base85', value: 'base-85', disabled: false },
   ];
 
   const encode = useCallback(() => {
-    if ('base16' === type) {
-      setTarget(enc.Hex.stringify(enc.Utf8.parse(plain)));
-    } else if ('base32' === type) {
-    } else if ('base58' === type) {
-      const uint8array = new TextEncoder().encode(plain);
-      const result = bs58.encode(Uint8Array.from(uint8array));
-      console.log(bs58check.encode(uint8array));
-      setTarget(result);
-      const base58 = basex('123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ');
-      console.log(base58.encode(uint8array));
-    } else if ('base64' === type) {
-      setTarget(enc.Base64.stringify(enc.Utf8.parse(plain)));
-    }
+    const result = baseEncode(type, plain);
+    setTarget(result);
   }, [type, plain]);
 
   const decode = useCallback(() => {
-    if ('base16' === type) {
-      setTarget(enc.Utf8.stringify(enc.Hex.parse(plain)));
-    } else if ('base32' === type) {
-    } else if ('base58' === type) {
-      const result = new TextDecoder().decode(bs58.decode(plain));
-      setTarget(result);
-    } else if ('base64' === type) {
-      setTarget(enc.Utf8.stringify(enc.Base64.parse(plain)));
-    }
+    const result = baseDecode(type, plain);
+    setTarget(result);
   }, [type, plain]);
 
   return (
