e5d657b18ddf927e2f55089e055bdc31ac89ba2f
liuzhaowei55 <liuzhaowei55@gmail.com>
2024-01-03 23:44:14 +0800

    feat: 开启 pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
