0f4072bee18c9aecc78dff8c43d52ec72d6c7072
liuzhaowei55 <liuzhaowei55@gmail.com>
2024-01-03 22:16:20 +0800

    feat: loading

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,15 +1,9 @@
-import { Box, Stack } from '@mui/system';
 import { Spin } from 'antd';
 
 export default function Loading() {
   return (
-    <Stack direction="row" justifyContent="center" alignItems="center" spacing={2} sx={{ height: '100vh' }}>
-      <Box>
-        <Spin />
-      </Box>
-      <Box>
-        <div>加载中...</div>
-      </Box>
-    </Stack>
+    <Spin tip="加载中...">
+      <div className="w-full h-[100vh]" />
+    </Spin>
   );
 }
