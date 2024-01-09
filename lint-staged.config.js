5f7887ac73ba47c85a5bee64a635436186297fa9
liuzhaowei55 <liuzhaowei55@gmail.com>
2024-01-09 23:41:47 +0800

    feat: pkg update

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,3 @@
 export default {
   'package.json': ['prettier --write'],
-  '**/*.css': ['stylelint --fix', 'prettier --write --ignore-unknown'],
-  '*.md': 'prettier --parser markdown --write --ignore-unknown',
-  '**/*.js?(x)': (filenames) =>
-    filenames.map((filename) => {
-      return [`eslint --fix '${filename}'`, `prettier --write --ignore-unknown '${filename}'`];
-    }),
-  '**/*.ts?(x)': (filenames) =>
-    filenames.map((filename) => {
-      return [`eslint --fix '${filename}'`, `prettier --parser typescript --write --ignore-unknown '${filename}'`];
-    }),
 };
