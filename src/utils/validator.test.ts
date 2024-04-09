18646e984a36d534ad7356710ce9153faeeb8f52
liuzhaowei55 <liuzhaowei55@gmail.com>
2024-04-09 20:03:10 +0800

    feat: remove joi

--- src/utils/validator.test.ts
+++ src/utils/validator.test.ts
@@ -1,9 +1,20 @@
-import { isUnixSecond } from '@/utils/validator.ts';
+import { isMillisecond, isUnixSecond } from '@/utils/validator.ts';
 import { describe, expect, it } from 'vitest';
 
 describe('isUnixSecond', () => {
   it('normal number', () => {
     const result = isUnixSecond(9);
+    expect(result).toEqual(true);
+  });
+  it('负数', () => {
+    const result = isUnixSecond(-9);
+    expect(result).toEqual(false);
+  });
+});
+
+describe('isMillisecond', () => {
+  it('normal number', () => {
+    const result = isMillisecond(new Date().getTime());
     expect(result).toEqual(true);
   });
 });
--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,35 +1,19 @@
 import { isMatch } from 'date-fns';
-import * as JOI from 'joi';
 import z from 'zod';
 
 export const isUnixSecond = (num: string | number): boolean => {
   const schema = z.union([z.string().regex(/^[0-9]{10}$/), z.number().int().min(0)]);
-  const result = schema.safeParse(num);
-  return result.success;
+  return schema.safeParse(num).success;
 };
 
 export const isMillisecond = (num: string | number): boolean => {
-  const scheme = JOI.string()
-    .pattern(/^[0-9]{13}$/)
-    .required();
-  try {
-    JOI.assert(num, scheme);
-    return true;
-  } catch (e) {
-    return false;
-  }
+  const schema = z.union([z.string().regex(/^[0-9]{13}$/), z.number().int().min(0)]);
+  return schema.safeParse(num).success;
 };
 
 export const isNumber = (num: string | number): boolean => {
-  const scheme = JOI.string()
-    .pattern(/^[0-9]+$/)
-    .required();
-  try {
-    JOI.assert(num, scheme);
-    return true;
-  } catch (e) {
-    return false;
-  }
+  const schema = z.union([z.string().regex(/^[0-9]+$/), z.number().int().min(0)]);
+  return schema.safeParse(num).success;
 };
 
 export const isDateStr = (str: string, format: string): boolean => {
