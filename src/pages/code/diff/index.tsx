174f1d51678624d691e6fccc99e1b876136b02cd
liuzhaowei55 <liuzhaowei55@gmail.com>
2024-04-19 00:57:24 +0800

    feat: pkg update

--- src/pages/code/diff/index.tsx
+++ src/pages/code/diff/index.tsx
@@ -1,4 +1,6 @@
+import { PageContainer } from '@/components/ui';
 import { EditorState } from '@codemirror/state';
+import { githubLight } from '@uiw/codemirror-theme-github';
 import { Card } from 'antd';
 import { EditorView } from 'codemirror';
 import type { FC } from 'react';
@@ -14,9 +16,9 @@ five`;
 
 export const Component: FC = () => {
   return (
-    <div className="h-dvh p-4">
+    <PageContainer>
       <Card className="h-full">
-        <CodeMirrorMerge orientation="a-b" revertControls="a-to-b">
+        <CodeMirrorMerge orientation="a-b" revertControls="a-to-b" theme={githubLight} lang="plain">
           <Original value={doc} extensions={[EditorView.editable.of(true), EditorState.readOnly.of(false)]} />
           <Modified
             value={doc.replace(/t/g, 'T') + 'Six'}
@@ -24,6 +26,6 @@ export const Component: FC = () => {
           />
         </CodeMirrorMerge>
       </Card>
-    </div>
+    </PageContainer>
   );
 };
