307e0197898f4ec6956fefb962f027edeb919e63
liuzhaowei55 <liuzhaowei55@gmail.com>
2024-05-01 17:57:42 +0800

    feat: i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,31 +1,30 @@
 import type { InitOptions } from 'i18next';
 import i18next from 'i18next';
 import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector';
-import I18NextHttpBackend from 'i18next-http-backend';
+import ChainedBackend from 'i18next-chained-backend';
+import resourcesToBackend from 'i18next-resources-to-backend';
 import { initReactI18next } from 'react-i18next';
 
 const initOptions: InitOptions = {
+  load: 'languageOnly',
   backend: {
-    // 網頁載入時去下載語言檔的位置
-    loadPath: '/locales/{{lng}}/{{ns}}.json',
+    backends: [resourcesToBackend((lng: string, ns: string) => import(`./locales/${lng}/${ns}.json`))],
   },
   // lng: 'zh-Hans', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
   // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
   // if you're using a language detector, do not define the lng option
   fallbackLng: {
-    default: ['zh-CN'],
+    en: ['en'],
     zh: ['zh-CN'],
+    default: ['zh-CN'],
   },
-  supportedLngs: ['zh-CN', 'en'],
   interpolation: {
     escapeValue: false, // react already safes from xss
   },
 };
 
 i18next
-  .use(I18NextHttpBackend)
   .use(I18nextBrowserLanguageDetector)
+  .use(ChainedBackend)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init(initOptions);
-
-export default i18next;
