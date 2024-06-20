7178dfc8a42d835d631c223567578c7ccedf4690
liuzhaowei55 <liuzhaowei55@gmail.com>
2024-06-20 20:21:22 +0800

    feat: remove eslint prettier

--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-import alloy from 'eslint-config-alloy/.prettierrc.js';
-
-/** @type {import("prettier").Config} */
-export default {
-  ...alloy,
-  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-packagejson', 'prettier-plugin-tailwindcss'],
-};
