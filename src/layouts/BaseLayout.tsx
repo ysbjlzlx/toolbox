726033cc1598558c4c73f9549c61953339018fa9
liuzhaowei55 <liuzhaowei55@gmail.com>
2024-07-10 22:13:27 +0800

    feat(sidebar): 侧边栏折叠优化

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,12 +1,12 @@
 import type { MenuDataItem, ProLayoutProps, ProSettings } from "@ant-design/pro-components";
 import { ProConfigProvider, ProLayout } from "@ant-design/pro-components";
-import { App, ConfigProvider } from "antd";
+import { useBoolean } from "ahooks";
+import { App, Button, ConfigProvider } from "antd";
 import type { FC, ReactNode } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
 
 import Iconify from "@/components/Iconify";
 import { menuData } from "@/layouts/defaultProps";
-import { Outlet } from "react-router-dom";
 
 const settings: Partial<ProSettings> = {
   fixSiderbar: true,
@@ -25,18 +25,36 @@ const menuItemRender = (item: MenuDataItem, dom: ReactNode) => {
   return <Link to={item.path}>{dom}</Link>;
 };
 
-const actionsRender: ProLayoutProps["actionsRender"] = (props) => {
-  if (props.isMobile) return [];
-  return [
-    <Link to="https://github.com/ysbjlzlx/toolbox/tree/develop" aria-label="GitHub" target="_blank" key="GitHub">
-      <Iconify icon="lucide:github" fontSize={24} color="#000" />
-    </Link>,
-  ];
-};
-
 const BaseLayout: FC = () => {
   const location = useLocation();
   const navigate = useNavigate();
+  const [collapsed, { toggle }] = useBoolean(false);
+
+  const actionsRender: ProLayoutProps["actionsRender"] = (props) => {
+    const { isMobile } = props;
+    if (isMobile) return [];
+    return [
+      <Link to="https://github.com/ysbjlzlx/toolbox/tree/develop" aria-label="GitHub" target="_blank" key="GitHub">
+        <Iconify icon="lucide:github" fontSize={24} color="#000" />
+      </Link>,
+    ];
+  };
+
+  const collapsedButtonRender: ProLayoutProps["collapsedButtonRender"] = (collapsed) => {
+    return (
+      <>
+        {collapsed ? (
+          <Button ghost onClick={toggle}>
+            <Iconify icon="lucide:chevrons-right" fontSize={24} color="#000" />
+          </Button>
+        ) : (
+          <Button ghost onClick={toggle}>
+            <Iconify icon="lucide:chevrons-left" fontSize={24} color="#000" />
+          </Button>
+        )}
+      </>
+    );
+  };
 
   const onMenuHeaderClick = () => {
     navigate("/");
@@ -44,9 +62,12 @@ const BaseLayout: FC = () => {
   const proLayoutProps: ProLayoutProps = {
     logo: "/logo.svg",
     title: "Toolbox",
+    collapsed,
     menuItemRender,
     onMenuHeaderClick,
     actionsRender,
+    collapsedButtonRender,
+    onCollapse: toggle,
     location: location,
     menu: {
       request: async () => {
