27e3c23defc5a425c862fb75d9cce7b64d1e016f
liuzhaowei55 <liuzhaowei55@gmail.com>
2024-07-30 00:30:40 +0800

    feat: pwa upgrade

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { registerSW } from "virtual:pwa-register";
 import { StyleProvider } from "@ant-design/cssinjs";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
@@ -10,6 +11,8 @@ import router from "@/router.tsx";
 import "@/i18n";
 
 import "./globals.css";
+
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,61 +1,62 @@
 /// <reference types="vitest" />
 
-import react from '@vitejs/plugin-react';
-import path from 'node:path';
-import { defineConfig } from 'vite';
-import type { VitePWAOptions } from 'vite-plugin-pwa';
-import { VitePWA } from 'vite-plugin-pwa';
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import type { VitePWAOptions } from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa";
 
 const pwaOptions: Partial<VitePWAOptions> = {
-  registerType: 'autoUpdate',
-  includeAssets: ['logo.png', 'logo.svg', 'favicon.ico'],
+  injectRegister: "auto",
+  registerType: "autoUpdate",
+  includeAssets: ["logo.png", "logo.svg", "favicon.ico"],
   manifest: {
-    name: 'Toolbox',
-    short_name: 'Toolbox',
-    start_url: '.',
-    display: 'standalone',
-    theme_color: '#ffffff',
-    background_color: '#ffffff',
+    name: "Toolbox",
+    short_name: "Toolbox",
+    start_url: ".",
+    display: "standalone",
+    theme_color: "#ffffff",
+    background_color: "#ffffff",
     icons: [
       {
-        src: 'favicon.ico',
-        sizes: '48x48',
-        type: 'image/x-icon',
+        src: "favicon.ico",
+        sizes: "48x48",
+        type: "image/x-icon",
       },
       {
-        src: 'icons/icon-x48.png',
-        type: 'image/png',
-        sizes: '48x48',
+        src: "icons/icon-x48.png",
+        type: "image/png",
+        sizes: "48x48",
       },
       {
-        src: 'icons/icon-x72.png',
-        type: 'image/png',
-        sizes: '72x72',
+        src: "icons/icon-x72.png",
+        type: "image/png",
+        sizes: "72x72",
       },
       {
-        src: 'icons/icon-x96.png',
-        type: 'image/png',
-        sizes: '96x96',
+        src: "icons/icon-x96.png",
+        type: "image/png",
+        sizes: "96x96",
       },
       {
-        src: 'icons/icon-x128.png',
-        type: 'image/png',
-        sizes: '128x128',
+        src: "icons/icon-x128.png",
+        type: "image/png",
+        sizes: "128x128",
       },
       {
-        src: 'icons/icon-x192.png',
-        type: 'image/png',
-        sizes: '192x192',
+        src: "icons/icon-x192.png",
+        type: "image/png",
+        sizes: "192x192",
       },
       {
-        src: 'icons/icon-x384.png',
-        type: 'image/png',
-        sizes: '384x384',
+        src: "icons/icon-x384.png",
+        type: "image/png",
+        sizes: "384x384",
       },
       {
-        src: 'icons/icon-x512.png',
-        type: 'image/png',
-        sizes: '512x512',
+        src: "icons/icon-x512.png",
+        type: "image/png",
+        sizes: "512x512",
       },
     ],
   },
@@ -64,7 +65,7 @@ export default defineConfig({
   plugins: [react(), VitePWA(pwaOptions)],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   build: {
@@ -72,10 +73,10 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          antd: ['antd'],
-          'pro-components': ['@ant-design/pro-components'],
-          ace: ['ace-builds', 'react-ace'],
-          jsoneditor: ['vanilla-jsoneditor'],
+          antd: ["antd"],
+          "pro-components": ["@ant-design/pro-components"],
+          ace: ["ace-builds", "react-ace"],
+          jsoneditor: ["vanilla-jsoneditor"],
         },
       },
     },
@@ -84,9 +85,9 @@ export default defineConfig({
     host: true,
   },
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
     globals: true,
-    setupFiles: './src/setupTests.ts',
+    setupFiles: "./src/setupTests.ts",
     css: true,
   },
 });
