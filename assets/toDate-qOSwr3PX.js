c303517da6151ba2698bc5b40fe5865ec2f6f2b6
liuzhaowei55 <liuzhaowei55@users.noreply.github.com>
2024-09-13 14:42:37 +0000

    deploy: 4a370c8013453e59e11bddfd126f212ccf0ebb51

---
+++ assets/toDate-qOSwr3PX.js
@@ -0,0 +1,1 @@
+function o(t){const e=Object.prototype.toString.call(t);return t instanceof Date||typeof t=="object"&&e==="[object Date]"?new t.constructor(+t):typeof t=="number"||e==="[object Number]"||typeof t=="string"||e==="[object String]"?new Date(t):new Date(NaN)}export{o as t};
---
+++ assets/translation-C4rynA82.js
@@ -0,0 +1,1 @@
+const o="Home",t={Home:o,"JSON Editor":"JSON Editor"};export{o as Home,t as default};
