3e34a6492d3111a09a87d87154d111a88611de40
liuzhaowei55 <liuzhaowei55@gmail.com>
2024-10-08 23:21:34 +0800

    feat: password

--- src/pages/password/index.tsx
+++ src/pages/password/index.tsx
@@ -1,15 +1,28 @@
 import { PageContainer } from "@ant-design/pro-components";
-import { Card, Tabs } from "antd";
-import type { TabsProps } from "antd";
+import { Card } from "antd";
+import type { CardProps } from "antd";
+import { useState } from "react";
+import type React from "react";
 
 import { Pin } from "./pin";
 
+const tabList: CardProps["tabList"] = [{ label: "# PIN", key: "pin" }];
+
 export const Component = () => {
-  const tabsItems: TabsProps["items"] = [{ label: "# PIN", key: "pin", children: <Pin /> }];
+  const [activeTabKey, setActiveTabKey] = useState<string>("pin");
+
+  const contentList: Record<string, React.ReactNode> = {
+    pin: <Pin />,
+  };
+
+  const onTabChange: CardProps["onTabChange"] = (key) => {
+    setActiveTabKey(key);
+  };
+
   return (
     <PageContainer title="密码" className="p-4">
-      <Card>
-        <Tabs items={tabsItems} />
+      <Card tabList={tabList} onTabChange={onTabChange} activeTabKey={activeTabKey}>
+        {contentList[activeTabKey]}
       </Card>
     </PageContainer>
   );
