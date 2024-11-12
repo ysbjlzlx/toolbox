743b50787d94cb1bd7c4d2b364c69b43e40328d1
liuzhaowei55 <liuzhaowei55@gmail.com>
2024-11-13 00:14:14 +0800

    feat: json parse string

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,17 @@
-import typography from '@tailwindcss/typography';
-import type { Config } from 'tailwindcss';
+import * as typography from "@tailwindcss/typography";
+import type { Config } from "tailwindcss";
 
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     screens: {
-      xs: '480px',
-      sm: '576px',
-      md: '768px',
-      lg: '992px',
-      xl: '1200px',
-      '2xl': '1600px',
+      xs: "480px",
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      "2xl": "1600px",
     },
   },
   plugins: [typography],
