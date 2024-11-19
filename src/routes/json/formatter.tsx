0e20d743db07bb0fb7c6e1df68a4313271847e0e
liuzhaowei55 <liuzhaowei55@gmail.com>
2024-11-19 21:44:29 +0800

    feat: add monaco-editor

--- src/routes/json/formatter.tsx
+++ src/routes/json/formatter.tsx
@@ -2,16 +2,28 @@
 
 import useJSONFormatterStore from "@/stores/JSONFormatterStore.ts";
 import { PageContainer } from "@ant-design/pro-components";
-import { Editor, type OnChange, type OnMount } from "@monaco-editor/react";
+import { Editor } from "@monaco-editor/react";
+import type { Monaco, OnChange, OnMount } from "@monaco-editor/react";
 import type { editor } from "monaco-editor";
-import { type FC, useRef } from "react";
+import { type FC, useEffect, useRef } from "react";
 
 export const Component: FC = () => {
   const editorRef = useRef<editor.IStandaloneCodeEditor>();
-  const { value, setValue } = useJSONFormatterStore();
+  const monacoRef = useRef<Monaco>();
+  const { value, setValue, options } = useJSONFormatterStore();
 
-  const handleEditorDidMount: OnMount = (editor, _monaco) => {
+  useEffect(() => {
+    editorRef.current?.updateOptions(options);
+  }, [options]);
+
+  const handleEditorDidMount: OnMount = (editor, monaco) => {
     editorRef.current = editor;
+    monacoRef.current = monaco;
+
+    monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+      validate: true,
+      allowComments: true,
+    });
   };
 
   const handleEditorChange: OnChange = (value, _event) => {
@@ -24,8 +36,19 @@ export const Component: FC = () => {
       <Editor
         height="100dvh"
         defaultLanguage="json"
-        options={{ tabSize: 2, formatOnPaste: true, formatOnType: true }}
-        theme="vs-dark"
+        options={{
+          tabSize: 2,
+          formatOnPaste: true,
+          formatOnType: true,
+          stickyTabStops: true,
+          stickyScroll: {
+            enabled: true,
+            maxLineCount: 15,
+            scrollWithEditor: true,
+            defaultModel: "foldingProviderModel",
+          },
+        }}
+        theme="light"
         defaultValue={value || "// some json"}
         onMount={handleEditorDidMount}
         onChange={handleEditorChange}
--- src/stores/JSONFormatterStore.ts
+++ src/stores/JSONFormatterStore.ts
@@ -1,18 +1,26 @@
+import type { editor } from "monaco-editor";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
 interface JSONFormatterStore {
   value: string | null;
+  options: editor.IEditorOptions & editor.IGlobalEditorOptions;
   setValue: (value: string) => void;
+  setOptions: (options: editor.IStandaloneEditorConstructionOptions) => void;
 }
 
 const useJSONFormatterStore = create<JSONFormatterStore>()(
   persist(
     (set) => ({
       value: null,
+      options: {},
       setValue: (value: string) =>
         set(() => {
           return { value: value };
+        }),
+      setOptions: (options) =>
+        set(() => {
+          return { ...options };
         }),
     }),
     { name: "json-formatter", storage: createJSONStorage(() => localStorage) },
