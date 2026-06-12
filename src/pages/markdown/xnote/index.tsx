0500e1f9ac244a88d28d0f9ab0cf6b80a9a191ff
liuzhaowei55 <liuzhaowei55@gmail.com>
2026-06-12 15:05:20 +0800

    feat: xnote

--- src/pages/markdown/xnote/index.tsx
+++ src/pages/markdown/xnote/index.tsx
@@ -1,31 +1,38 @@
 import { PageContainer } from "@ant-design/pro-components";
-import { Editor, InlineToolbarPlugin } from "@textbus/xnote";
+import { Editor, InlineToolbarPlugin, LeftToolbarPlugin } from "@textbus/xnote";
 import { type FC, useEffect, useRef } from "react";
 
 import "katex/dist/katex.min.css";
 
 export const Component: FC = () => {
-  const editorRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const editorRef = useRef<Editor>(undefined);
 
   useEffect(() => {
-    const editor = new Editor({
-      plugins: [new InlineToolbarPlugin({ theme: "dark" })],
-    });
-    if (editorRef?.current) {
-      editor.mount(editorRef.current).then(() => {
+    if (containerRef?.current) {
+      editorRef.current = new Editor({
+        plugins: [new InlineToolbarPlugin({ theme: "dark" }), new LeftToolbarPlugin({ theme: "dark" })],
+      });
+      editorRef.current.mount(containerRef.current).then(() => {
         console.log("编辑器准备完成。");
       });
     }
     return () => {
-      console.log(editor.getHTML());
-      editor.destroy();
+      console.log(editorRef.current?.getHTML());
+      editorRef.current?.destroy();
     };
   }, []);
 
   return (
     <PageContainer title={false} className="p-4 pt-10">
-      <div className="h-[calc(100dvh-112px)] md:h-[calc(100dvh-56px)]">
-        <div ref={editorRef} className="vditor" />
+      <div className="editor-main">
+        <div className="editor-center">
+          <div className="editor-card">
+            <h3 className="editor-title">行内工具条 + 左侧工具条</h3>
+            <div ref={containerRef} className="editor-host"></div>
+            <div className="editor-scroll-spacer"></div>
+          </div>
+        </div>
       </div>
     </PageContainer>
   );
