d79650e73af50eb42a0b7903b6d3afa298373348
liuzhaowei55 <liuzhaowei55@gmail.com>
2026-06-25 15:33:24 +0800

    feat: react-router

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { registerSW } from "virtual:pwa-register";
 import { StyleProvider } from "@ant-design/cssinjs";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 
 import { routes } from "@/routes.tsx";
 
