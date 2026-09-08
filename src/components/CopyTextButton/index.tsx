8e6d0ff2c98a3788218381c93cd532d33d733bb9
liuzhaowei55 <liuzhaowei55@gmail.com>
2026-09-08 21:15:02 +0800

    feat: icon

--- src/components/CopyTextButton/index.tsx
+++ src/components/CopyTextButton/index.tsx
@@ -1,9 +1,9 @@
 import type { ButtonProps } from "antd";
 import { App, Button } from "antd";
 import copy from "copy-to-clipboard";
+import { Copy as CopyIcon } from "lucide-react";
 import type React from "react";
 import type { FC } from "react";
-import Iconify from "@/components/Iconify";
 
 interface Props extends ButtonProps {
   text?: string | null;
@@ -23,7 +23,7 @@ const CopyTextButton: FC<Props> = ({ text, children, ...props }) => {
     }
   };
   return (
-    <Button icon={<Iconify icon="lucide:copy" />} onClick={onClick} {...props}>
+    <Button icon={<CopyIcon size={16} />} onClick={onClick} {...props}>
       {children}
     </Button>
   );
