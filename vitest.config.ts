6ff6074e9a4eea78b097bdef4a37b199a4ad0fcd
liuzhaowei55 <liuzhaowei55@gmail.com>
2026-09-08 20:33:38 +0800

    feat: vitest v5

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,41 @@
 import { defineConfig, mergeConfig } from "vitest/config";
-import viteConfig from "./vite.config";
+import viteConfig from "./vite.config.ts";
+import { playwright } from '@vitest/browser-playwright'
 
 const vitestConfig = defineConfig({
   test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./src/setupTests.ts",
-    css: true,
+    projects:[
+      {
+        test:{
+          environment: "jsdom",
+          globals: true,
+          setupFiles: "./src/setupTests.ts",
+          css: true,
+          include: [
+            'tests/unit/**/*.{test,spec}.ts',
+            'tests/**/*.unit.{test,spec}.ts',
+          ],
+        }},
+      {
+        test: {
+          // an example of file based convention,
+          // you don't have to follow it
+          include: [
+            'tests/browser/**/*.{test,spec}.ts',
+            'tests/browser/**/*.browser.{test,spec}.ts',
+            '**/*.browser.{test,spec}.tsx',
+          ],
+          name: 'browser',
+          browser: {
+            enabled: true,
+            provider: playwright(),
+            instances: [
+              { browser: 'chromium' },
+            ],
+          },
+        },
+      }
+    ],
   },
 });
 
