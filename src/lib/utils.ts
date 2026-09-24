8b1914b20ad16d19875be0d32696d46bd1e5b001
liuzhaowei55 <liuzhaowei55@gmail.com>
2026-09-24 12:11:09 +0800

    feat: add shadcn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,1 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export const cn = (...inputs: ClassValue[]) => {
-  return twMerge(clsx(inputs));
-};
+export { cn } from "cn";
