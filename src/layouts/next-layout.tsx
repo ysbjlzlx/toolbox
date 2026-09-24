2af3f4145bcadd7d52c2e6464dc78e6b72acdc9b
liuzhaowei55 <liuzhaowei55@gmail.com>
2026-09-24 17:54:58 +0800

    feat: add sidebar-08

--- src/layouts/next-layout.tsx
+++ src/layouts/next-layout.tsx
@@ -19,20 +19,20 @@ const NextLayouot: FC = () => {
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+          <Separator orientation="vertical" />
           <Breadcrumb>
             <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
+              <BreadcrumbItem className="md:block">
                 <BreadcrumbLink href="#">Build Your Application</BreadcrumbLink>
               </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbSeparator className="md:block" />
               <BreadcrumbItem>
                 <BreadcrumbPage>Data Fetching</BreadcrumbPage>
               </BreadcrumbItem>
             </BreadcrumbList>
           </Breadcrumb>
         </header>
-        <main>
+        <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
           <Outlet />
         </main>
       </SidebarInset>
